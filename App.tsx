36820894071536cc2d91e3cf74cc381a1fde062f
sorintf <sorin.dan@twoandfrom.com>
2023-09-19 13:16:10 +0300

    navigation - installing

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>
+          Hello
+        </Text>
+      </View>
+    </NavigationContainer>
+  )
+}
+
+export default App;
